1b2a364d88c9fa3ce825d327238da058e9878cc7
jazib.hassan <jazib.hassan@careem.com>
2020-08-18 22:38:30 +0500

    Added jpa model for likes

---
+++ src/main/java/com/crafting/our_twitter/repository/model/Likes.java
@@ -0,0 +1,30 @@
+package com.crafting.our_twitter.repository.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "likes")
+public class Likes {
+    @Id
+    private Integer id;
+
+    @Column(name = "tweet_id")
+    private String tweetId;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "reaction_enum")
+    private Integer reactionEnum;
+
+}
+
