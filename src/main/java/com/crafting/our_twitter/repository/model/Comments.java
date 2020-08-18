59a3c0f4d2fd67b9efe970bfffae92ff5f0eb8ba
jazib.hassan <jazib.hassan@careem.com>
2020-08-18 22:35:16 +0500

    Added jpa model for Comments

---
+++ src/main/java/com/crafting/our_twitter/repository/model/Comments.java
@@ -0,0 +1,31 @@
+package com.crafting.our_twitter.repository.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comments")
+public class Comments {
+
+    @Id
+    private String id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "tweet_id")
+    private Integer tweetId;
+
+    @Column(name = "timestamp")
+    private Timestamp timestamp;
+
+}
