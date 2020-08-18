c059673567351386d8ad33cf94f4f1c68be97246
jazib.hassan <jazib.hassan@careem.com>
2020-08-18 22:30:31 +0500

    Added jpa model for tweets

---
+++ src/main/java/com/crafting/our_twitter/repository/model/Tweets.java
@@ -0,0 +1,31 @@
+package com.crafting.our_twitter.repository.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tweets")
+public class Tweets {
+
+    @Id
+    private String id;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "timestamp")
+    private Timestamp timestamp;
+
+}
+
