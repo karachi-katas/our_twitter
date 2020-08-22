5407d00dc9df4fe460e3114f9c60cd2d9b888e62
Jazib Hassan <jazib.hassan@careem.com>
2020-08-22 16:54:11 +0500

    Connected with HomeController

--- src/main/java/com/crafting/our_twitter/rest/HomeController.java
+++ src/main/java/com/crafting/our_twitter/rest/HomeController.java
@@ -1,13 +1,25 @@
 package com.crafting.our_twitter.rest;
 
+import com.crafting.our_twitter.service.UserService;
+import javax.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/")
 public class HomeController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping(value = "hello")
     public String hello() {
         return "Hello Comrade";
+    }
+
+    @GetMapping(value = "login/{username}/{password}")
+    public Integer login(@PathVariable(value = "username") String username, @PathVariable(value = "password")String password) {
+        return userService.signIn(username, password);
     }
 }
